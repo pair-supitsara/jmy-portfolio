75b587b79b7241650287f2cb4092590dceb38929
pairsupit <pair.supitsara@gmail.com>
2024-09-22 22:54:25 +0700

    transition
    - add transition
    - open new tab (cv)

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,9 +11,9 @@ function Contact() {
                         </div>
                         <div className='sub-card-detail right'>
                             <span>Email : pair.supitsara@gmail.com</span>
-                            <span>Github : <a href="https://github.com/pair-supitsara">github.com/pair-supitsara</a></span>
-                            <span>Medium : <a href="https://medium.com/@supitsara.d">medium.com/@supitsara.d</a></span>
-                            <span>Resume : <a href="/CV_Supitsara_Deetoh.pdf">CV_Supitsara_Deetoh.pdf</a></span>
+                            <span>Github : <a href="https://github.com/pair-supitsara" target='_blank'>github.com/pair-supitsara</a></span>
+                            <span>Medium : <a href="https://medium.com/@supitsara.d" target='_blank'>medium.com/@supitsara.d</a></span>
+                            <span>Resume : <a href="/CV_Supitsara_Deetoh.pdf" target='_blank'>CV_Supitsara_Deetoh.pdf</a></span>
                         </div>
                     </div>
                 </div>
